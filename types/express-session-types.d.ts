c2cc571077f31e5e49768ad9c6ba6dfdab5efe7a
h2pl <hohoangphilong@gmail.com>
2021-04-29 16:49:57 +0700

    fix: lint, modules

--- types/express-session-types.d.ts
+++ types/express-session-types.d.ts
@@ -2,11 +2,10 @@
  * Naming this file express-session.d.ts causes imports from "express-session"
  * to reference this file and not the node_modules package.
  */
+import session from 'express-session';
 
-import session from "express-session-types";
-
-declare module "express-session" {
-    export interface SessionData {
-        returnTo: string;
-    }
+declare module 'express-session' {
+  interface SessionData {
+    returnTo: string;
+  }
 }
